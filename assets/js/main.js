7b7151fe943949941a56453f784738a09ff1231e
TanviSharma2104 <tanvisharma2703@gmail.com>
2023-10-25 20:00:35 +0530

    email form updated with api

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,14 +37,39 @@ window.addEventListener('scroll',scrollHeader)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections=document.querySelectorAll('section[id]');
 
-/*=============== SHOW SCROLL UP ===============*/ 
+const scrollActive=()=>{
+    const scrollY=window.scrollY
+    sections.forEach(current=>{
+        const sectionHeight=current.offsetHeight,
+            sectionTop=current.offsetTop-58,
+            sectionId=current.getAttribute('id'),
+            sectionsClass=document.querySelector('.nav__menu a[href*='+sectionId);
 
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ){
+            sectionsClass.classList.add('active-link')
+        }
+        else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll',scrollActive);
+/*SHOW SCROLL UP */ 
 
+const scrollUp=()=>{
+    const scrollUp=document.getElementById('scroll-up');
+    //add show-scroll class is scroll is heigher than 350 vw
+    this.scrollY>=350? scrollUp.classList.add('show-scroll')
+                    :scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll',scrollUp);
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
 
-/*=============== CALCULATE JS ===============*/
+/*CALCULATE JS */
 const calculateForm=document.getElementById('calculate-form'),
     calculateCm=document.getElementById('calculate-cm'),
     calculateKg=document.getElementById('calculate-kg'),
@@ -100,4 +125,51 @@ const calculateBmi=(e)=>{
 
 calculateForm.addEventListener('submit',calculateBmi);
 
-/*=============== EMAIL JS ===============*/
+/* EMAIL JS*/
+
+const contactForm=document.getElementById('contact-form'),
+    contactMessage=document.getElementById('contact-message'),
+    contactUser=document.getElementById('contact-user');
+
+const sendEmail=(e)=>{
+    e.preventDefault();
+    //check if the field has a value
+
+    if(contactUser.value===''){
+        //add and remove color
+        contactMessage.classList.remove('color-green');
+        contactMessage.classList.add('color-red');
+
+        //show error message
+        contactMessage.textContent="You must enter your email address";
+
+        //remove message after 3 seconds
+        setTimeout(()=>{
+            contactMessage.textContent=''
+        },3000);
+    }
+    else{
+        contactMessage.classList.add('color-green');
+        contactMessage.textContent="You have registered successfully"
+        //api serviceID -templateID -#form -publicKey
+        //emailjs.sendForm(serviceID, templateID, templateParams, publicKey);
+        emailjs.sendForm('service_giu4019','template_q2r9ief','#contact-form','oPSv7GwY4vOezJ_9q')
+            .then(()=>{
+                //show message and add color
+                // contactMessage.classList.add('color-green');
+                // contactMessage.textContent="You have registered successfully"
+
+                //remove message after 3 seconds
+                setTimeout(()=>{
+                    contactMessage.textContent='';
+                },3000)
+            },(error)=>
+            {
+                //Mail sending error
+                alert('OOPS! Something went wrong ....',error)
+            })
+            contactUser.value='';
+    }
+}
+
+contactForm.addEventListener('submit',sendEmail);
