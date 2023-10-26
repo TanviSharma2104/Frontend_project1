e363aab707feb9eea9690946860b67790a2595b1
TanviSharma2104 <tanvisharma2703@gmail.com>
2023-10-26 14:16:12 +0530

    Login/SignUp page added

---
+++ assets/js/signIn_signUp.js
@@ -0,0 +1,27 @@
+function toggleSignup(){
+    document.getElementById("login-toggle").style.backgroundColor="#fff";
+     document.getElementById("login-toggle").style.color="#222";
+     document.getElementById("signup-toggle").style.backgroundColor=" hsl(79, 63%, 50%)";
+     document.getElementById("signup-toggle").style.color="#fff";
+     document.getElementById("login-form").style.display="none";
+     document.getElementById("signup-form").style.display="block";
+ }
+ 
+ function toggleLogin(){
+     document.getElementById("login-toggle").style.backgroundColor=" hsl(79, 63%, 50%)";
+     document.getElementById("login-toggle").style.color="#fff";
+     document.getElementById("signup-toggle").style.backgroundColor="#fff";
+     document.getElementById("signup-toggle").style.color="#222";
+     document.getElementById("signup-form").style.display="none";
+     document.getElementById("login-form").style.display="block";
+ }
+
+
+ //scroll down animation
+ const scr=ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:2500,
+    delay:400,
+})
+scr.reveal(`.form-modal`);
